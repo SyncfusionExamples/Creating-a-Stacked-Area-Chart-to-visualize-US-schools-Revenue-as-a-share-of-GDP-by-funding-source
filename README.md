7b673c0c608bfbf59bf1b368a107e053700ba8d0
aruljenithberkmans <aruljenith.berkmans@syncfusion.com>
2023-12-19 16:29:17 +0530

    Update the sample and README.md file

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Creating-a-Stacked-Area-chart-to-visualize-US-schools-Revenue-as-a-share-of-GDP-by-funding-source
-This sample demonstrates how to create a .Net Maui Stacked Area Charts for Revenue of US primary and secondary schools, as share of GDP by source of funds
+# Creating-a-Stacked-Area-Chart-to-visualize-US-schools-Revenue-as-a-share-of-GDP-by-funding-source
+This sample demonstrates how to create a .Net Maui Stacked Area Charts for Revenue of US primary and secondary schools, as share of GDP by source of funds.
+
+## Stacked Area 
+The stacked Area chart is a dynamic visualization that allows us to observe changes in values over time while highlighting the contributions of each component. This chart type is particularly effective in displaying the proportional distribution of revenue source, giving us an understandable view of the financial landscape. 
 
 
 ![StackedAreaChartSample](https://github.com/SyncfusionExamples/Creating-a-Stacked-Area-Chart-to-visualize-US-schools-Revenue-as-a-share-of-GDP-by-funding-source/assets/124584591/dc026449-ab34-49ca-b308-467e95109f72)
---
+++ USSchoolsRevenue/ViewModel/FoundingSourceViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USSchoolsRevenue
+{
+    public class FoundingSourceViewModel
+    {
+        private ObservableCollection<FundingSourceModel> fundingSourceCollection = new ObservableCollection<FundingSourceModel>();
+
+        public ObservableCollection<FundingSourceModel> FundingSourceCollection
+        {
+            get
+            {
+                return fundingSourceCollection;
+            }
+            set
+            {
+                fundingSourceCollection = value;
+            }
+        }
+
+        public FoundingSourceViewModel()
+        {
+            FundingSourceCollection = new ObservableCollection<FundingSourceModel>();
+            ReadCSV();
+        }
+
+        public void ReadCSV()
+        {
+            Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
+            Stream? inputStream = executingAssembly.GetManifestResourceStream("USSchoolsRevenue.Resources.raw.data.csv");
+            string? line;
+            List<string> lines = new List<string>();
+            if (inputStream != null)
+            {
+                using StreamReader reader = new StreamReader(inputStream);
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+                lines.RemoveAt(0);
+                foreach (var dataPoint in lines)
+                {
+                    string[] data = dataPoint.Split(',');
+
+                    string year = data[0];
+                    var localFund = Convert.ToDouble(data[1]);
+                    var stateFund = Convert.ToDouble(data[2]);
+                    var federalFund = Convert.ToDouble(data[3]);
+                    FundingSourceCollection.Add(new FundingSourceModel(year, localFund, stateFund, federalFund));
+                }
+            }
+        }
+
+    }
+}
